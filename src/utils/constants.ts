c99c41e1a07f25ea800bc5687e53a7ad2b799679
Nuno Carvalho <nunoormaker21@gmail.com>
2024-03-22 15:21:14 +0000

    chore: separate helpers and constants; create placeholder for campaign tiers

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,41 +1,5 @@
 import { Month } from "./types";
 
-export const computeTier = (
-  metric: keyof typeof METRICS_TRANSLATED,
-  value: number
-): string => {
-  const tier = sortedTiers.find((tier) => {
-    return value >= TIER_METRIC_LIMITS[tier][metric];
-  });
-  return tier;
-};
-
-export const computeLineaTier = (
-  metric: keyof typeof METRICS_TRANSLATED,
-  value: number
-): string => {
-  const tier = sortedLineaTiers.find((tier) => {
-    return value >= LINEA_TIER_METRIC_LIMITS[tier][metric];
-  });
-  return tier;
-};
-
-export const computePoints = (metric: string, tier: string): number => {
-  return TIER_DATA[metric]["points"][tier] || 0;
-};
-
-export const computeLineaPoints = (metric: string, tier: string): number => {
-  return LINEA_TIER_DATA[metric]["points"][tier] || 0;
-};
-
-export const formatTier = (text: string): string => {
-  if (text == "baby") return "Novice";
-
-  return text
-    .split("_")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-};
 export const ORG_ID = process.env.ORG_ID!;
 export const LOYALTY_DM_ID = process.env.LOYALTY_DM_ID!;
 
---
+++ src/utils/helpers.ts
@@ -0,0 +1,56 @@
+import {
+  LINEA_TIER_DATA,
+  LINEA_TIER_METRIC_LIMITS,
+  METRICS_TRANSLATED,
+  TIER_DATA,
+  TIER_METRIC_LIMITS,
+  sortedLineaTiers,
+  sortedTiers,
+} from "./constants";
+import { Campaign } from "./types";
+
+export const computeTier = (
+  metric: keyof typeof METRICS_TRANSLATED,
+  value: number
+): string => {
+  const tier = sortedTiers.find((tier) => {
+    return value >= TIER_METRIC_LIMITS[tier][metric];
+  });
+  return tier;
+};
+
+export const computeLineaTier = (
+  metric: keyof typeof METRICS_TRANSLATED,
+  value: number
+): string => {
+  const tier = sortedLineaTiers.find((tier) => {
+    return value >= LINEA_TIER_METRIC_LIMITS[tier][metric];
+  });
+  return tier;
+};
+
+export const computeCampaignTier = (
+  campaign: Campaign,
+  value: number
+): string => {
+  // TODO: Implement this function
+
+  return "TODO";
+};
+
+export const computePoints = (metric: string, tier: string): number => {
+  return TIER_DATA[metric]["points"][tier] || 0;
+};
+
+export const computeLineaPoints = (metric: string, tier: string): number => {
+  return LINEA_TIER_DATA[metric]["points"][tier] || 0;
+};
+
+export const formatTier = (text: string): string => {
+  if (text == "baby") return "Novice";
+
+  return text
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
